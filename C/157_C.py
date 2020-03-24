34d5cd5736284016aa505b96e06fec1ed87c6ec0
Takahiro800 <52635809+Takahiro800@users.noreply.github.com>
2020-03-24 19:20:18 +0900

    157C

--- C/157_C.py
+++ C/157_C.py
@@ -5,4 +5,40 @@ M = int(t[1])
 
 y = [input() for i in range(M)]
 
+#import pdb; pdb.set_trace()
 #まだ未完了
+l = [-1] * N
+i = 0
+ans = 0
+
+if ('1 0' in y) and (N != 1):
+  ans = -1
+
+while i < M:
+  y[i] = y[i].split()
+  a = int(y[i][0])
+  b = int(y[i][1])
+
+  if  l[a-1] == -1:
+    l[a-1] = b
+  elif l[a-1] != b:
+    ans = -1
+    break
+  i += 1
+
+#import pdb; pdb.set_trace()
+
+while -1 in l:
+  k  = l.index(-1)
+  l[k] = 0
+
+if (l[0] == 0) and (N != 1):
+  l[0] = 1
+
+if ans == 0:
+  k = N
+  while k > 0:
+    ans += l[-k] * (10 ** (k-1))
+    k -= 1
+
+print(ans)
