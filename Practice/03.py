9d73bad9276cc5ad172ac450260bd434072cf496
Takahiro800 <52635809+Takahiro800@users.noreply.github.com>
2020-03-26 11:14:54 +0900

    精選10問No.3

--- Practice/03.py
+++ Practice/03.py
@@ -1,13 +1,27 @@
-m = input()
-m = list(m)
+N = int(input())
+A = input().split()
 
+i = 0
 ans = 0
-k = 0
 
-while k < 3:
-  if m[k] == '1':
+even_count = N
+
+while even_count == N:
+  even_count = 0
+  i = 0
+
+  while i < N:
+    a = int(A[i])
+    if a % 2 == 0:
+      even_count += 1
+      A[i] = a // 2
+      i += 1
+    else:
+      break
+
+  if even_count == N:
     ans += 1
-
-  k += 1
+  else:
+    break
 
 print(ans)
