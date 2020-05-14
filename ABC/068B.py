829b531ca3fedea3c7f8f06621052d84465c295c
Takahiro Abe <hirotaka081630@gmail.com>
2020-05-15 05:55:53 +0900

    068B

---
+++ ABC/068B.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+if N >= 64:
+  print(64)
+elif N >=32:
+  print(32)
+elif N >= 16:
+  print(16)
+elif N >= 8:
+  print(8)
+elif N >= 4:
+  print(4)
+elif N >= 2:
+  print(2)
+else:
+  print(1)
