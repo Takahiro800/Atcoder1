1700c502102fb1ada7d232467d3590d48a1e47b5
Takahiro Abe <hirotaka081630@gmail.com>
2020-05-16 06:47:43 +0900

    122B

---
+++ ABC/B/122B.py
@@ -0,0 +1,18 @@
+S = input()
+N = len(S)
+a,b = 0,0
+l = [0]
+
+for i in range(N):
+  if S[i] == 'A' or S[i] == 'C' or S[i] == 'G' or S[i] == 'T':
+    a += 1
+    b = 1
+  else:
+    if b:
+      l.append(a)
+      b = 0
+      a = 0
+else:
+  if a:
+    l.append(a)
+  print(max(l))
