709c796c4d722be34d219fdd7a655676b9e6be00
Takahiro Abe <hirotaka081630@gmail.com>
2020-08-08 11:14:27 +0900

    js 入門

--- ABC/practice.js
+++ ABC/practice.js
@@ -1,14 +1,15 @@
+// inputに入力データ全体が入る
 function Main(input) {
-  input = input.split("\n");
-  tmp = input[1].split(" ");
-
-  //文字列から10進数に変換するときはparseInt(input[0], 10);
-  const a = parseInt(input[0], 10);
-  const b = parseInt(tmp[0], 10);
-  const c = parseInt(tmp[1], 10);
-  const s = input[2];
-
-  console.log('%d %s', a+b+c, s);
+	// 1行目がinput[0], 2行目がinput[1], …に入る
+	input = input.split("\n");
+	tmp = input[1].split(" ");
+	//文字列から10進数に変換するときはparseIntを使います
+	const a = parseInt(input[0], 10);
+	const b = parseInt(tmp[0], 10);
+	const c = parseInt(tmp[1], 10);
+	const s = input[2];
+	//出力
+	console.log('%d %s',a+b+c,s);
 }
-
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
