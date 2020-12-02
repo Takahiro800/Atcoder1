b5c70725cc9d4e2e6905fb4ce43cdad796164188
Takahiro Abe <hirotaka081630@gmail.com>
2020-12-02 20:21:18 +0900

    ファイル整理

---
+++ ABC/AGC002A.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b int
+	fmt.Scan(&a, &b)
+
+	if a <= 0 && b>= 0 {
+		fmt.Println("Zero")
+	} else if a > 0 || (b-a+1)%2 == 0 {
+		fmt.Println("Positive")
+	} else {
+		fmt.Println("Negative")
+	}
+}
---
+++ ABC/readme.md
@@ -0,0 +1,7 @@
+# Go学習メモ
+## 標準入力
+
+```go
+var x, n int
+fmt.Scan(&x, &n)
+```
